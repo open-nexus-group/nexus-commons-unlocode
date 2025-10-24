50b3b51c5cd483c6d2d13846825eb7159247bd06
Otto Mamestsarashvili <mrtunii@outlook.com>
2025-10-24 16:10:06 +0400

    added search by proximity and function

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/open-nexus-group/nexus-commons-unlocode/pkg/data"
 	"github.com/open-nexus-group/nexus-commons-unlocode/pkg/unlocode"
 )
 
@@ -31,6 +32,18 @@ func main() {
 	log.Printf("direct by Name: Shanghai Hongqiao International Apt: %v", locationByName)
 	log.Printf("found by Locode: CNSH: %v", foundByCode)
 	log.Printf("found by Name: shanghai: %v", foundByName)
+
+	foundByNameAndProximity, err := locodeCollection.FindByNameAndProximity("Hamburg", 3, 0.8)
+	if err != nil {
+		log.Printf("Could not findByNameAndProximity: %v", err)
+	}
+	log.Printf("found by Name and Proximity: Hamburg: %v", foundByNameAndProximity)
+
+	foundByNameAndProximityWithFunction, err := locodeCollection.FindByNameAndProximityWithFunction("Hamburg", 3, 0.8, data.PORT)
+	if err != nil {
+		log.Printf("Could not findByNameAndProximityWithFunction: %v", err)
+	}
+	log.Printf("found by Name and Proximity with Function: Hamburg: %v", foundByNameAndProximityWithFunction)
 	elapsed := time.Since(start)
 	log.Printf("All operations took %s", elapsed)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/open-nexus-group/nexus-commons-unlocode 
+module github.com/open-nexus-group/nexus-commons-unlocode
 
 go 1.24.5
 
 require (
-	github.com/dave/jennifer v1.7.1 // indirect
-	github.com/hbollon/go-edlib v1.6.0 // indirect
+	github.com/dave/jennifer v1.7.1
+	github.com/hbollon/go-edlib v1.6.0
 )
